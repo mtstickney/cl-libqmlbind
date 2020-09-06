75057d7964b59063d8a436b34af31fc5caec196c
Matthew Stickney <fiendishlinux@gmail.com>
2020-09-06 14:25:33 -0400

    Expect the generation script to be run from the root directory.
    Turns out `(cd scripts/; ./generate.sh)` gets tiresome to type after a
    while, so just assume this will be run from the project root.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,7 +5,7 @@ if [ -z "$QMLBIND_HEADERS" ]; then
 fi
 
 if [ -f "$QMLBIND_HEADERS/qmlbind.h" ]; then
-	BINDING_FILE=bindings.gen.i
+	BINDING_FILE=scripts/bindings.gen.i
 	cat > "$BINDING_FILE" <<EOF
 %feature("intern_function", "lispify");
 
@@ -17,7 +17,7 @@ EOF
 	cat "$QMLBIND_HEADERS/qmlbind.h" >> "$BINDING_FILE"
 	sed -i -e 's/#include/%include/' "$BINDING_FILE"
 else
-	BINDING_FILE=libqmlbind.i
+	BINDING_FILE=scripts/libqmlbind.i
 fi
 
-swig -cffi -noswig-lisp -o ../ffi.lisp -module ffi -DQMLBIND_API -I"$QMLBIND_HEADERS" "$BINDING_FILE"
+swig -cffi -noswig-lisp -o ffi.lisp -module ffi -DQMLBIND_API -I"$QMLBIND_HEADERS" "$BINDING_FILE"
