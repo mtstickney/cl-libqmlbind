feaff282423c0ea55c75f87093dedc85b8800ea2
Matthew Stickney <fiendishlinux@gmail.com>
2020-09-06 14:03:08 -0400

    Use common header to drive generation.
    qmlbind, at least in newer versions, ships with a single header that
    includes the others in the project. SWIG can't process that file directly
    (it doesn't recurse into #includes), but we can pre-process it a bit to
    turn it into a valid binding file, and not have to worry about maintaining
    the include list.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,4 +4,20 @@ if [ -z "$QMLBIND_HEADERS" ]; then
 	QMLBIND_HEADERS=/usr/include
 fi
 
-swig -cffi -noswig-lisp -o ../ffi.lisp -module ffi -DQMLBIND_API -I"$QMLBIND_HEADERS" libqmlbind.i
+if [ -f "$QMLBIND_HEADERS/qmlbind.h" ]; then
+	BINDING_FILE=bindings.gen.i
+	cat > "$BINDING_FILE" <<EOF
+%feature("intern_function", "lispify");
+
+%insert("lisphead") %{
+(in-package #:cl-libqmlbind)
+%}
+
+EOF
+	cat "$QMLBIND_HEADERS/qmlbind.h" >> "$BINDING_FILE"
+	sed -i -e 's/#include/%include/' "$BINDING_FILE"
+else
+	BINDING_FILE=libqmlbind.i
+fi
+
+swig -cffi -noswig-lisp -o ../ffi.lisp -module ffi -DQMLBIND_API -I"$QMLBIND_HEADERS" "$BINDING_FILE"
